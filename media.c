2cbba4183f38d4a1305c58807ca93610e72c8bd4
gleysonJr01 <gleysonairton01@gmail.com>
2025-09-13 02:10:53 +0000

    Commit do desafio do super trunfo

---
+++ media.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int populacao = 10;
+    int populacao_carta2 = 15;
+    printf("Comparação de Cartas:\n\n");
+    printf("populaçao: Carta_1: %d | Carta_2: %d\n", populacao > populacao_carta2, populacao<populacao_carta2);
+
+    return 0;
+}
--- super_trunfo.c
+++ super_trunfo.c
@@ -4,10 +4,13 @@ int main(){
     char estado,estado_carta2;
     char codigo[5], codigo_carta2[5];
     char nome_cidade[30],nome_cidade_carta2[30];
-    int populacao,populacao_carta2;
+    unsigned long int populacao,populacao_carta2;
     float area,area_carta2;
     float pib,pib_carta2;
     int pontos_turisticos,pontos_turisticos_carta2;
+    float densidade_popu, densidade_popu2;
+    float pib_capita,pib_capita2;
+    float super_poder, super_poder2;
 //Essa é o código para a carta 1
 
     printf("Essa é a Carta 1. Por favor preencha as informações dessa carta:\n");
@@ -32,6 +35,10 @@ int main(){
     printf("Digite quantos pontos turísticos tem nessa cidade:\n");
         scanf("%d", &pontos_turisticos);
 
+    densidade_popu = populacao/area;
+    pib_capita = pib/populacao;
+    super_poder = (float) populacao+area+pib+pontos_turisticos+(1/densidade_popu)+pib_capita;
+
 // Esse é o código para a carta 2
     
     printf("Essa é a Carta 2. Por favor preencha as informações dessa carta:\n");
@@ -54,10 +61,14 @@ int main(){
         scanf("%f", &pib_carta2);
 
     printf("Digite quantos pontos turísticos tem nessa cidade:\n");
-        scanf("%d", &pontos_turisticos_carta2);
+        scanf("%d\n\n", &pontos_turisticos_carta2);
+
+    densidade_popu2= populacao_carta2/area_carta2;
+    pib_capita2= pib_carta2/populacao_carta2;
+    super_poder2 = (float) populacao_carta2+area_carta2+pib_carta2+pontos_turisticos_carta2+(1/densidade_popu2)+pib_capita2;
 
 //Agora aqui exibi tudo oque foi inserido pelo o usuário
-printf("Aqui estão as informações que voccê inseriu para cada carta:\n\n ");
+printf("Aqui estão as informações que voccê inseriu para cada carta:===============================\n ");
 printf("Carta: %d\n", num_carta);
 printf("Estado: %c\n", estado);
 printf("Código: %s\n", codigo);
@@ -65,8 +76,9 @@ printf("Nome da cidade: %s\n", nome_cidade);
 printf("População: %d\n", populacao);
 printf("Área: %.2f\n", area);
 printf("PIB: %.2f\n", pib);
-printf("Número de Pontos Turísticos: %d\n\n", pontos_turisticos);
-
+printf("Número de Pontos Turísticos: %d\n", pontos_turisticos);
+printf("Densidade Populacional: %f\n", densidade_popu);
+printf("PIB per capita: %f\n\n", pib_capita);
 
 printf("Carta: %d\n", num_carta2);
 printf("Estado: %c\n", estado_carta2);
@@ -74,7 +86,19 @@ printf("Código: %s\n", codigo_carta2);
 printf("Nome da cidade: %s\n", nome_cidade_carta2);
 printf("População: %d\n", populacao_carta2);
 printf("Área: %.2f\n", area_carta2);
-printf("PIB: %,2f\n", pib_carta2);
+printf("PIB: %.2f\n", pib_carta2);
 printf("Número de Pontos Turísticos: %d\n", pontos_turisticos_carta2);
+printf("Densidade Populacional: %f\n", densidade_popu2);
+printf("PIB per capita: %f\n\n", pib_capita2);
+
+printf("Comparação de Cartas: ===================================================\n\n");
+printf("populaçao: Carta_1: %d | Carta_2: %d\n", populacao > populacao_carta2, populacao<populacao_carta2);
+printf("Área: Carta_1: %d | Carta_2: %d\n", area > area_carta2, area<area_carta2);
+printf("PIB: Carta_1: %d | Carta_2: %d\n", pib > pib_carta2, pib<pib_carta2);
+printf("Pontos turisticos: Carta_1: %d | Carta_2: %d\n", pontos_turisticos > pontos_turisticos_carta2, pontos_turisticos<pontos_turisticos_carta2);
+printf("Densidade Populacional: Carta_1: %d | Carta_2: %d\n", densidade_popu > densidade_popu2, densidade_popu<densidade_popu2);
+printf("PIB per capita: Carta_1: %d | Carta_2: %d\n", pib_capita > pib_capita2, pib_capita<pib_capita2);
+printf("Super Poder: Carta_1: %d | Carta_2: %d\n", super_poder > super_poder2, super_poder<super_poder2);
+
     return 0;
 }
